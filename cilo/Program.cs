1e332b19ca69b014f2348362389f3b240e4e1dd6
sylla <kh-sylla@live.jp>
2019-09-12 00:26:02 +0900

    カーソル移動とスクロールを実装。カーソルとスクロール情報を Document 管理とする。（マルチドキュメンとやりたい。）

--- cilo/Program.cs
+++ cilo/Program.cs
@@ -103,8 +103,9 @@ namespace cilo
             }
         }
 
-        public int RowsCount => Console.WindowHeight;
-        public int ColumnsCount => Console.WindowWidth;
+        public int Height => Console.WindowHeight;
+        public int Width => Console.BufferWidth;
+        public Size Size => new Size(this.Width, this.Height);
 
         public ConsoleKeyInfo ReadKey()
         {
@@ -121,65 +122,103 @@ namespace cilo
             this.Write("\x1b[2J"); // Console.Clear();
             this.Write("\x1b[H"); // Console.SetCursorPosition(0, 0);
         }
+
+        public void RefreshScreen(Action<IScreenBuffer> drawEditorRows, Point cursor)
+        {
+            var buffer = new Buffer(this.Size);
+
+            buffer.AppendHideCursorSequence();
+            buffer.AppendSetCursorPositionZeroSequence();
+            drawEditorRows(buffer);
+            buffer.AppendSetCursorPositionSequence(cursor);
+            buffer.AppendShowCursorSequence();
+
+            this.Write(buffer.ToString());
+        }
+
+        class Buffer : IScreenBuffer
+        {
+            readonly StringBuilder buffer;
+            readonly Size size;
+
+            int rowCount;
+
+            public Buffer(Size size)
+            {
+                this.buffer = new StringBuilder();
+                this.size = size;
+
+                this.rowCount = 0;
+            }
+
+            public void AppendRow(string value)
+            {
+                this.buffer.Append(value);
+                this.AppendClearLineSequence();
+                if (this.rowCount < this.size.Height - 1)
+                {
+                    this.AppendNewLineSequence();
+                }
+                ++this.rowCount;
+            }
+
+            void AppendClearLineSequence()
+            {
+                this.buffer.Append("\x1b[K");
+            }
+
+            void AppendNewLineSequence()
+            {
+                this.buffer.Append("\r\n");
+            }
+
+            public void AppendSetCursorPositionZeroSequence()
+            {
+                this.buffer.Append("\x1b[H");
+                // Console.SetCursorPosition(0, 0);
+            }
+            public void AppendSetCursorPositionSequence(Point cursor)
+            {
+                this.AppendSetCursorPositionSequence(cursor.X, cursor.Y);
+            }
+            public void AppendSetCursorPositionSequence(int x, int y)
+            {
+                this.buffer.Append($"\x1b[{y + 1};{x + 1}H");
+                // Console.SetCursorPosition(cursor.X, cursor.Y);
+            }
+
+            public void AppendHideCursorSequence()
+            {
+                this.buffer.Append("\x1b[?25l");
+            }
+            public void AppendShowCursorSequence()
+            {
+                this.buffer.Append("\x1b[?25h");
+            }
+
+            public override string ToString() => this.buffer.ToString();
+        }
     }
-
-    class Buffer
+    interface IScreenBuffer
     {
-        readonly StringBuilder buffer;
-
-        public Buffer()
-        {
-            this.buffer = new StringBuilder();
-        }
-
-        public void Append(string value)
-        {
-            this.buffer.Append(value);
-        }
-
-        public void AppendClearSequence()
-        {
-            this.buffer.Append("\x1b[2J");
-            // Console.Clear();
-        }
-
-        public void AppendClearLineSequence()
-        {
-            this.buffer.Append("\x1b[K");
-        }
-
-        public void AppendSetCursorPositionZeroSequence()
-        {
-            this.buffer.Append("\x1b[H");
-            // Console.SetCursorPosition(0, 0);
-        }
-        public void AppendSetCursorPositionSequence(Point cursor)
-        {
-            this.buffer.Append($"\x1b[{cursor.Y + 1};{cursor.X + 1}H");
-            // Console.SetCursorPosition(cursor.X, cursor.Y);
-        }
-
-        public void AppendHideCursorSequence()
-        {
-            this.buffer.Append("\x1b[?25l");
-        }
-        public void AppendShowCursorSequence()
-        {
-            this.buffer.Append("\x1b[?25h");
-        }
-
-        public override string ToString() => this.buffer.ToString();
+        void AppendRow(string value);
     }
 
     class Document
     {
         public List<string> Rows { get; }
-        public int RowCount { get; set; }
+        public Point Cursor => new Point(this.position.X - this.offset.X, this.position.Y - this.offset.Y);
+        public Point Position => this.position;
+        public Point Offset => this.offset;
+
+        Point position;
+        Point offset;
 
         public Document()
         {
             this.Rows = new List<string> { };
-            this.RowCount = 0;
+            this.position = new Point(0, 0);
+            this.offset = new Point(0, 0);
         }
 
         public static Document Open(string path)
@@ -190,13 +229,128 @@ namespace cilo
                 while (reader.ReadLine() is var line && line != null)
                 {
                     doc.Rows.Add(line);
-                    doc.RowCount++;
                 }
 
             }
 
             return doc;
         }
+
+        public void MoveLeft()
+        {
+            if (this.position.X > 0)
+            {
+                --this.position.X;
+            }
+            else if (this.position.Y > 0)
+            {
+                --this.position.Y;
+                if (this.position.Y < this.Rows.Count)
+                {
+                    this.position.X = this.Rows[this.position.Y].Length;
+                }
+            }
+            ClampPosition();
+        }
+        public void MoveRight()
+        {
+            if (this.position.Y < this.Rows.Count)
+            {
+                var row = this.Rows[this.position.Y];
+                if (this.position.X < row.Length)
+                {
+                    ++this.position.X;
+                }
+                else
+                {
+                    this.position.X = 0;
+                    ++this.position.Y;
+                }
+            }
+            else
+            {
+                this.position.X = 0;
+            }
+            ClampPosition();
+        }
+        public void MoveUp()
+        {
+            if (this.position.Y > 0) { --this.position.Y; }
+            ClampPosition();
+        }
+        public void MoveDown() {
+            ++this.position.Y;
+            ClampPosition();
+        }
+        void ClampPosition()
+        {
+            if (this.position.Y < this.Rows.Count)
+            {
+                var row = this.Rows[this.position.Y];
+                if (this.position.X > row.Length) { this.position.X = row.Length; }
+            }
+            else
+            {
+                this.position.X = 0;
+                this.position.Y = this.Rows.Count;
+            }
+        }
+
+        public void MoveHome()
+        {
+            this.position.X = 0;
+        }
+        public void MoveEnd()
+        {
+            if (this.position.Y < this.Rows.Count)
+            {
+                this.position.X = this.Rows[this.position.Y].Length;
+            }
+            else
+            {
+                this.position.X = 0;
+                this.position.Y = this.Rows.Count;
+            }
+        }
+        public void MoveBeginOfFile()
+        {
+            this.position.X = 0;
+        }
+        public void MoveEndOfFile()
+        {
+            if (this.Rows.Count > 0)
+            {
+                var lastRowIndex = Rows.Count - 1;
+                this.position.X = this.Rows[lastRowIndex].Length;
+                this.position.Y = lastRowIndex;
+            }
+            else
+            {
+                this.position.X = 0;
+                this.position.Y = 0;
+            }
+        }
+
+        public void UpdateOffset(EditorConsole console)
+        {
+            if (this.position.Y < this.offset.Y)
+            {
+                this.offset.Y = this.position.Y;
+            }
+            if (this.position.Y >= this.offset.Y + console.Height)
+            {
+                this.offset.Y = this.position.Y - console.Height + 1;
+            }
+            if (this.position.X < this.offset.X)
+            {
+                this.offset.X = this.position.X;
+            }
+            if (this.position.X >= this.offset.X + console.Width)
+            {
+                this.offset.X = this.position.X - console.Width + 1;
+            }
+        }
+
     }
 
     class Editor
@@ -205,18 +359,16 @@ namespace cilo
 
         readonly EditorConsole console;
 
-        Point cursor;
-        Document docment;
+        Document document;
 
         public Editor(EditorConsole console)
         {
             this.console = console;
-            this.cursor = new Point(0, 0);
         }
 
         public void SetDocument(Document document)
         {
-            this.docment = document;
+            this.document = document;
         }
 
         public void Run()
@@ -236,57 +388,48 @@ namespace cilo
         }
 
 
-        public void RefreshScreen()
+
+        void RefreshScreen()
         {
-            var buffer = new Buffer();
-
-            buffer.AppendHideCursorSequence();
-            //buffer.AppendClearSequence();
-            buffer.AppendSetCursorPositionZeroSequence();
-            this.DrawEditorRows(buffer);
-            buffer.AppendSetCursorPositionSequence(this.cursor);
-            buffer.AppendShowCursorSequence();
-
-            this.console.Write(buffer.ToString());
+            this.document.UpdateOffset(this.console);
+            this.console.RefreshScreen(this.DrawEditorRows, this.document.Cursor);
         }
 
-        public void DrawEditorRows(Buffer buffer)
+        void DrawEditorRows(IScreenBuffer buffer)
         {
-            for (int y = 0; y < this.console.RowsCount; y++)
+            for (int y = 0; y < this.console.Height; ++y)
             {
-                if (y >= this.docment.RowCount)
+                var docRow = y + this.document.Offset.Y;
+                if (docRow < this.document.Rows.Count)
                 {
-                    if (this.docment.RowCount == 0 && y == this.console.RowsCount / 3)
-                    {
-                        var welcome = $"Cilo editor -- version {Version}";
-                        var welcomeLength = Math.Min(welcome.Length, this.console.ColumnsCount);
-                        var padding = (this.console.ColumnsCount - welcomeLength) / 2;
-                        if (padding > 0)
-                        {
-                            buffer.Append("~");
-                        }
-                        for (int i = 1; i < padding; i++)
-                        {
-                            buffer.Append(" ");
-                        }
-                        buffer.Append(welcome.Substring(0, welcomeLength));
-                    }
-                    else
-                    {
-                        buffer.Append("~");
-                    }
+                    var docText = this.document.Rows[docRow];
+                    var docLength = Math.Clamp(docText.Length - this.document.Offset.X, 0, this.console.Width);
+                    buffer.AppendRow(docLength >0 ? docText.Substring(this.document.Offset.X, docLength) : "");
                 }
                 else
                 {
-                    var docText = this.docment.Rows[y];
-                    var docLength = Math.Min(docText.Length, this.console.ColumnsCount);
-                    buffer.Append(docText.Substring(0, docLength));
-                }
-                
-                buffer.AppendClearLineSequence();
-                if (y < this.console.RowsCount - 1)
-                {
-                    buffer.Append("\r\n");
+                    if (this.document.Rows.Count == 0 && y == this.console.Height / 3)
+                    {
+                        var welcome = $"Cilo editor -- version {Version}";
+                        var welcomeLength = Math.Min(welcome.Length, this.console.Width);
+                        var padding = (this.console.Width - welcomeLength) / 2;
+                        var rowBuffer = new StringBuilder();
+                        if (padding > 0)
+                        {
+                            rowBuffer.Append("~");
+                        }
+                        for (int i = 1; i < padding; i++)
+                        {
+                            rowBuffer.Append(" ");
+                        }
+                        rowBuffer.Append(welcome.Substring(0, welcomeLength));
+
+                        buffer.AppendRow(rowBuffer.ToString());
+                    }
+                    else
+                    {
+                        buffer.AppendRow("~");
+                    }
                 }
             }
         }
@@ -301,10 +444,10 @@ namespace cilo
             switch (key.Key)
             {
                 case ConsoleKey.Home:
-                    this.cursor.X = 0;
+                    this.document.MoveHome();
                     return EditorProcessingResults.Running;
                 case ConsoleKey.End:
-                    this.cursor.X = console.ColumnsCount;
+                    this.document.MoveEnd();
                     return EditorProcessingResults.Running;
 
                 case ConsoleKey.PageUp:
@@ -324,7 +467,7 @@ namespace cilo
 
         public EditorProcessingResults MovePage(ConsoleKey key)
         {
-            for (int i = 0; i < this.console.RowsCount; i++) {
+            for (int i = 0; i < this.console.Height; ++i) {
                 this.MoveCursor(key);
             }
 
@@ -336,19 +479,18 @@ namespace cilo
             switch (key)
             {
                 case ConsoleKey.LeftArrow:
-                    if (this.cursor.X > 0) { this.cursor.X--; }
+                    this.document.MoveLeft();
                     break;
                 case ConsoleKey.RightArrow:
-                    if (this.cursor.X < this.console.ColumnsCount - 1) { this.cursor.X++; }
+                    this.document.MoveRight();
                     break;
                 case ConsoleKey.UpArrow:
-                    if (this.cursor.Y > 0) { this.cursor.Y--; }
+                    this.document.MoveUp();
                     break;
                 case ConsoleKey.DownArrow:
-                    if (this.cursor.Y < this.console.RowsCount - 1) { this.cursor.Y++; }
+                    this.document.MoveDown();
                     break;
             }
-
             return EditorProcessingResults.Running;
         }
     }
