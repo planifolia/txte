adc6af57ab0821dd2309d70ab2257d723458b21c
planifolia <kh-sylla@live.jp>
2019-09-21 21:03:03 +0900

    .net core のみを使用するようにする。

--- txte/Program.cs
+++ txte/Program.cs
@@ -7,19 +7,12 @@ namespace txte
 
         static int Main(string[] args)
         {
-            //Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
             try
             {
                 var setting = new EditorSetting();
                 var document =
                     (args.Length >= 1) ? Document.Open(args[0], setting)
                     : new Document();
-#if ESCAPE_SEQUENCE_ENHANCED
-                var console = new ConsoleWithEscapeSequence();
-#else
-                var console = new CoreConsole();
-#endif
                 var editor = new Editor(console, setting);
                 editor.SetDocument(document);
                 editor.SetStatusMessage("HELP: Ctrl-Q to quit, Alt-A to switch EAW ambiguous width...");
